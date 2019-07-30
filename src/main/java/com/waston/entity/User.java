4910e3ab6bf09f4342e69cf8f9783187280eb6e7
Yubao-wss <a371134638@163.com>
2019-07-30 22:07:54 +0800

    update_2019-7-30

---
+++ src/main/java/com/waston/entity/User.java
@@ -0,0 +1,15 @@
+package com.waston.entity;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: Waston
+ * @Date: 2019/7/30 21:59
+ */
+@Data
+public class User {
+    private Integer id;
+    private String userName;
+    private String password;
+}
---
+++ src/main/java/com/waston/utils/CommUtils.java
@@ -0,0 +1,34 @@
+package com.waston.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @Description: 封装基础的工具方法，如加载配置文件、json序列化等
+ * @Author: Waston
+ * @Date: 2019/7/30 19:04
+ */
+public class CommUtils {
+    private CommUtils(){}
+
+    /**
+     * 根据指定的文件名加载配置文件
+     * @param fileName
+     * @return
+     */
+    public static Properties loadProperties(String fileName){
+        Properties properties = new Properties();
+        try {
+
+            //获取当前配置文件夹下的输入流
+            InputStream in = CommUtils.class.getClassLoader().getResourceAsStream(fileName);
+            //加载配置文件里的所有内容
+            properties.load(in);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
